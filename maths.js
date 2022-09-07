cbe633c00f18045df9390fdae55fc9a171f43a15
Richard Lovell <cassh3rn@talktalk.net>
2022-09-07 19:29:06 +0100

    first commit

---
+++ maths.js
@@ -0,0 +1,53 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function square(num) {
+    return num * num;
+}
+
+function cube(num) {
+    return num * num * num;
+}
+
+function fib(n) {
+    if (n < 2) {
+        return n;
+    } 
+    return fib(n-1) + fib(n-2);
+}
+
+function isPrime(num) {
+    for(i=2; i <= (num / 2); i++) {
+        if (num % i === 0 && i !== num) {
+            return false
+        }
+    }
+    return true;
+}
+
+function firstNPrimes(n) {
+    let count = 0;
+    let list = [];
+    for(let j=2; count <= n; j++) {
+        if (isPrime(j)) {
+            list.push(j);
+            count++;
+        }
+    }
+    return list;
+}
+
+console.log(firstNPrimes(50));
